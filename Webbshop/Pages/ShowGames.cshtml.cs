ce2fe264d85ade3f3709f35a1a5f0f183a1a948d
GitCasperHub <92301578+GitCasperHub@users.noreply.github.com>
2021-11-12 11:21:29 +0100

    Sortering
    Sortering möjlighet på Games

--- Webbshop/Pages/ShowGames.cshtml.cs
+++ Webbshop/Pages/ShowGames.cshtml.cs
@@ -13,9 +13,26 @@ namespace Webbshop.Pages
     public class ShowGamesModel : PageModel
     {
         public List<Game> Games = GameManager.GetGames();
+
+        [BindProperty(SupportsGet = true)]
+        public string Sort { get; set; }
         public void OnGet()
         {
             Games = Games.OrderBy(game => game.Name).ToList();
+
+            if (Sort == "Price")
+            {
+                Games = Games.OrderBy(game => game.Price).ToList();
+            }
+            else if (Sort == "A2Z")
+            {
+                Games = Games.OrderBy(game => game.Name).ToList();
+            }
+            else if (Sort == "Z2A")
+            {
+                Games = Games.OrderByDescending(game => game.Name).ToList();
+            }
+
         }
     }
 }
